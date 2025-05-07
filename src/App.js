24cc8dfc1390c69bf8a07eea5d25e0fed4b70ec7
Gemnet <ssomani.k@gmail.com>
2025-05-07 09:12:11 -0400

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,13 +1,94 @@
 import React from 'react';
-import stemzLogo from './stemz-logo.png'; // Importing the logo image
+import stemzLogo from './stemz-logo.png'; // Import logo image
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       {/* Header Section */}
       <header className="header flex justify-between items-center p-4 bg-blue-700 shadow-md">
         {/* Logo */}
         <img src={stemzLogo} alt="Stemz Logo" className="logo w-20 h-auto" />
+
+        {/* SVG Divider */}
+        <svg
+          width="1131"
+          height="4049"
+          viewBox="0 0 1131 4049"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="divider"
+        >
+          <g clipPath="url(#clip0_1_2)">
+            <rect width="1131" height="4049" fill="white" />
+            <g clipPath="url(#clip1_1_2)">
+              <line
+                x1="452.5"
+                y1="-0.00697607"
+                x2="455.5"
+                y2="214.993"
+                stroke="black"
+              />
+              <line
+                x1="574.5"
+                y1="-0.00697607"
+                x2="577.5"
+                y2="214.993"
+                stroke="black"
+              />
+              <line
+                x1="674.5"
+                y1="-0.00697607"
+                x2="677.5"
+                y2="214.993"
+                stroke="black"
+              />
+              <line
+                x1="788.5"
+                y1="-0.00697607"
+                x2="791.5"
+                y2="214.993"
+                stroke="black"
+              />
+              <line
+                x1="876.5"
+                y1="-0.00697607"
+                x2="879.5"
+                y2="214.993"
+                stroke="black"
+              />
+              <line
+                x1="960.5"
+                y1="-0.00697607"
+                x2="963.5"
+                y2="214.993"
+                stroke="black"
+              />
+              <line
+                x1="1033.5"
+                y1="-0.00697607"
+                x2="1036.5"
+                y2="214.993"
+                stroke="black"
+              />
+            </g>
+            <rect y="-7" width="197" height="223" fill="#D9D9D9" />
+            <line
+              x1="340.5"
+              y1="-0.00697607"
+              x2="343.5"
+              y2="214.993"
+              stroke="black"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_1_2">
+              <rect width="1131" height="4049" fill="white" />
+            </clipPath>
+            <clipPath id="clip1_1_2">
+              <rect width="1131" height="215" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
 
         {/* Navigation Links */}
         <nav className="nav flex space-x-6">
@@ -21,7 +102,7 @@ function App() {
           <a href="#news" className="text-white hover:text-yellow-400 text-lg font-semibold transition-colors">News</a>
         </nav>
       </header>
-      
+
       {/* Main Content Section */}
       <main className="p-8 bg-gray-100">
         <section id="about" className="mb-8 bg-white p-6 rounded-lg shadow-md hover:bg-blue-50 transition-all">
@@ -66,6 +147,6 @@ function App() {
       </footer>
     </div>
   );
-}
+};
 
 export default App;
