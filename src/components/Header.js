e6fd8c147a3d44dcbefe769426ceac9a8cbcee2d
Gemnet <ssomani.k@gmail.com>
2025-05-11 14:13:14 -0400

    Your commit message

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,6 +23,7 @@ function Header() {
         <div
           className="hamburger"
           role="button"
+          aria-expanded={isMobileMenuOpen ? 'true' : 'false'}
           tabIndex={0}
           onClick={toggleMobileMenu}
           onKeyDown={(e) => {
@@ -38,8 +39,6 @@ function Header() {
             viewBox="0 0 448 512"
             height="20"
             width="20"
-            xmlns="http://www.w3.org/2000/svg"
-            style={{ color: 'black' }}
           >
             <path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163
               60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837
